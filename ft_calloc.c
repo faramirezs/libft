3c0fe943bc6748c5cbdd44ccfe4376ff0aba2e38
Alejandro Ramirez <alramire@c3a1c4.42berlin.de>
2024-05-09 15:21:40 +0200

    calloc testing

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,11 +6,12 @@
 /*   By: alramire <alramire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 10:57:52 by alramire          #+#    #+#             */
-/*   Updated: 2024/05/06 11:55:58 by alramire         ###   ########.fr       */
+/*   Updated: 2024/05/09 15:20:32 by alramire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdint.h>
 
 void	*ft_calloc(size_t count, size_t size)
 {
@@ -27,4 +28,29 @@ void	*ft_calloc(size_t count, size_t size)
 		i++;
 	}
 	return (tmp);
+}
+int	main(void)
+{
+
+	printf("=========Test 1 with zero======\n\n\n");
+	size_t	c;
+	size_t	s;
+
+	c = 0;
+	s = 0;
+
+	printf("Pointer address with ft %p\n", ft_calloc(c, s));
+	printf("Pointer address with std %p\n", calloc(c, s));
+
+	printf("=========Test 2 with overflow======\n\n\n");
+	size_t	co;
+	size_t	si;
+
+	co = 1;
+	si = SIZE_MAX;
+
+	printf("Pointer address with ft %s\n", ft_calloc(co, si));
+	printf("Pointer address with std %s\n", calloc(co, si));
+
+	return (0);
 }
